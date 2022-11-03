4cb6d0aafbc46f9b99aa00e2ce7152e7027a00cc
nazifa18 <nazifaafroz18@gmail.com>
2022-11-03 16:51:40 +0600

    new files added

--- resources/views/admin/foodmenu.blade.php
+++ resources/views/admin/foodmenu.blade.php
@@ -1,52 +1,116 @@
+<x-app-layout>
+
+</x-app-layout>
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <x-app-layout>
-
-    </x-app-layout>
+  <head>
+   
+   @include("admin.admincss")
+   
+  </head>
+  <body>
     
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        @include("admin.admincss")
-      </head>
-      <body>
-        <div class="container-scroller">
-        @include("admin.navbar")
+   <div class="container-scroller">
+     
+  	@include("admin.navbar")
+
+
+  	<div style="position: relative; top: 60px; right: -150px">
+  		
+  		<form action="{{url('/uploadfood')}}" method="post" enctype="multipart/form-data">
+
+  			@csrf
+
+  			
+  			<div>
+  				<label>Title</label>
+  				<input style="color:blue;" type="text" name="title" placeholder="Write a title" required>
+  			</div>
+
+  			<div>
+  				<label>Price</label>
+  				<input style="color:blue;" type="num" name="price" placeholder="price" required>
+  			</div>
+
+  			<div>
+  				<label>Image</label>
+  				<input type="file" name="image"  required>
+  			</div>
+
+  			<div>
+  				<label>Description</label>
+  				<input style="color:blue;" type="text" name="description" placeholder="Description" required>
+  			</div>
+
+  			<div>
+  				
+  				<input style="color: black" type="submit" value="Save" >
+  			</div>
+
+
+  		</form>
+
+
+
+      <div>
         
-        <div style="position: relative; top:60px; right: -150px">
-            <form action="{{ url('/uploadfood') }}" method="post" enctype="multipart/form-data">
-                @csrf
-                <div>
-                    <label for="">Title</label>
-                    <input style="color: blue;" type="text" name="title" id="" placeholder="Write a title" required>
-                </div>
-                <div>
-                    <label for="">Price</label>
-                    <input style="color: blue;" type="number" name="price" id="" placeholder="Price" required>
-                </div>
-                <div>
-                    <label for="">Image</label>
-                    <input type="file" name="image" id="" required>
-                </div>
-                <div>
-                    <label for="">Description</label>
-                    <input style="color: blue;" type="text" name="description" id="" placeholder="Write a Description" required>
-                </div>
-                <div>
-                    <input style="background-color: White; Color: black" type="submit" value="Save">
-                </div>
-            </form>
-        </div>
-        </div>
-        @include("admin.adminscript")
-      </body>
-    </html>
-</body>
+        <table bgcolor="black">
+          <tr>
+            <th style="padding: 30px">Food Name</th>
+            <th style="padding: 30px">Price</th>
+            <th style="padding: 30px">Description</th>
+            <th style="padding: 30px">Image</th>
+            <th style="padding: 30px">Action</th>
+          <th style="padding: 30px">Action2</th>
+            
+          </tr>
+
+
+
+          @foreach($data as $data)
+
+          <tr align="center">
+            
+            <td>{{$data->title}}</td>
+            <td>{{$data->price}}</td>
+            <td>{{$data->description}}</td>
+            <td><img height="200" width="200" src="/foodimage/{{$data->image}}"></td>
+
+            <td><a href="{{url('/deletemenu',$data->id)}}">Delete</a></td>
+
+            <td><a href="{{url('/updateview',$data->id)}}">Update</a></td>
+
+
+
+         
+
+          @endforeach
+
+
+        </table>
+
+
+      </div>
+
+
+
+
+
+
+
+  	</div>
+
+
+    
+
+
+
+</div>
+
+   @include("admin.adminscript")
+
+
+  </body>
 </html>
