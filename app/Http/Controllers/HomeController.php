62a227116cd748e7580ed4b21b12d8bda38948f2
nazifa18 <nazifaafroz18@gmail.com>
2022-11-05 21:42:55 +0600

    insertdata,insert multiple data and showorder data added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,8 @@ use App\Models\Foodchef;
 
 use App\Models\Cart;
 
+use App\Models\Order;
+
 
 class HomeController extends Controller
 {
@@ -92,9 +94,28 @@ class HomeController extends Controller
         {
             return redirect()->back();
         }
-}
+    }
 
+    public function remove($id)
+    {
+        $data=cart::find($id);
+        $data->delete();
+        return redirect()->back();
+    }
 
-
+    public function orderconfirm(Request $request)
+    {
+        foreach($request->foodname as $key =>$foodname){
+            $data=new order;
+            $data->foodname=$foodname;
+            $data->price=$request->price[$key];
+            $data->quantity=$request->quantity[$key];
+            $data->name=$request->name;
+            $data->phone=$request->phone;
+            $data->address=$request->address;
+            $data->save();
+        }
+            return redirect()->back();
+    }
 }
 
